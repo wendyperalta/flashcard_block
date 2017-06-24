9b77a37f61b1b7274f398ae509732fbe77e896d1
Wendy Peralta <wendy.prlt511@gmail.com>
2017-06-24 15:09:09 -0400

    building static version of app

--- app/components/App.js
+++ app/components/App.js
@@ -2,6 +2,7 @@ var React = require('react');
 var Flashcard = require('./Flashcard');
 
 var App = React.createClass({
+  
   // calculate how many flashcards to display here based on data.json
     
   render: function() {
--- app/index.js
+++ app/index.js
@@ -1,9 +1,39 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var App = require('./components/App');
+var PropTypes = require('prop-types'); //React.PropTypes is deprecated
+
+//Displays the entire app
+var App = React.createClass({
+  propTypes: {
+    flashcardsdata: PropTypes.array.isRequired
+  },
+
+  render: function() {
+    return <Flashcard flashcards={this.props.flashcardsdata} />;
+  }
+});
+
+var FLASHCARDSDATA = [
+    {
+        "type": "text", 
+        "front": "Front of Card",
+        "back": "Back of Card"
+    },
+    {
+        "type": "image",
+        "front": "Front of Card",
+        "back": "background-image",
+        "image": "coffee.jpg"
+    },
+    {
+        "type": "paragraph",
+        "front": "Front of Card",
+        "back": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit sapien lectus, eu varius tellus pharetra non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin auctor justo at facilisis suscipit. Fusce a posuere neque."
+    }
+];
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('app')
+  //App takes data model as a prop
+  <App flashcardsdata={FLASHCARDSDATA} />,
+  document.getElementById('container')
 );
-
